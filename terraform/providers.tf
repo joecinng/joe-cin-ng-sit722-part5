ed218c4c59d9ed6526af5d299611eb55d60d19eb
Joe Cin Ng <joe5763481@gmail.com>
2024-09-17 11:16:22 +1000

    change providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,10 +12,25 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
-    client_id        = var.ARM_CLIENT_ID
-    client_secret    = var.ARM_CLIENT_SECRET
-    tenant_id        = var.AZURE_TENANT_ID
-    subscription_id  = var.MVP_SUBSCRIPTION
+  features {}
+  client_id        = var.ARM_CLIENT_ID
+  client_secret    = var.ARM_CLIENT_SECRET
+  tenant_id        = var.AZURE_TENANT_ID
+  subscription_id  = var.MVP_SUBSCRIPTION
+
+  variable "tenant_id" {
+    type = string
+  }
+
+  variable "client_id" {
+    type = string
+  }
+
+  variable "client_secret" {
+    type = string
+  }
+
+  variable "subscription_id" {
+    type = string
   }
 }
