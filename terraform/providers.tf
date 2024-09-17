17cbd0c65e742caec990ba2a130a4f19668a7d0f
Joe Cin Ng <joe5763481@gmail.com>
2024-09-17 11:14:47 +1000

    change providers.tf

---
+++ terraform/providers.tf
@@ -0,0 +1,21 @@
+# Initialises Terraform providers and sets their version numbers.
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.71.0"
+    }
+  }
+
+  required_version = ">= 1.5.6"
+}
+
+provider "azurerm" {
+  features {
+    client_id        = var.ARM_CLIENT_ID
+    client_secret    = var.ARM_CLIENT_SECRET
+    tenant_id        = var.AZURE_TENANT_ID
+    subscription_id  = var.MVP_SUBSCRIPTION
+  }
+}
